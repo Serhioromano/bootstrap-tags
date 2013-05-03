faceaf29f0ef3a64f2cb3af47a9a295dfb9ba86c
serhioromano <serg4172@mail.ru>
2013-05-03 22:48:29 +0600

    Almost final
    now require jquery color plugin.

---
+++ urls/suggestions.json.php
@@ -0,0 +1,20 @@
+[
+    {
+        "id": "7",
+        "text": "Grapes",
+        "html": "This is <b>Grapes</b> html!",
+        "num": 5,
+        "url": "http://tags/",
+        "title": "This is test title and link"
+    },
+    {
+        "id": "7",
+        "text": "Apricot",
+        "html": "I like <b>Apricot</b>!",
+        "url": "/"
+    },
+    {
+        "id": "9",
+        "text": "Strawberry"
+    }
+]
